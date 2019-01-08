bd3315daca3260abb5c8f74bd53d4e8f6b0b11e4
Matteo Bertrone <m.bertrone@gmail.com>
2019-01-08 11:00:22 +0100

    pcn-iptables: fix HORUS when all rules are removed
    when all rules are removed, and HOURS is enabled,
    parser reloading is needed.
    
    Signed-off-by: Matteo Bertrone <m.bertrone@gmail.com>

---
+++ src/services/pcn-iptables/test/local_test_horus1.sh
@@ -0,0 +1,41 @@
+
+source "${BASH_SOURCE%/*}/helpers.bash"
+
+function iptablescleanup {
+    set +e
+    polycubectl iptables del pcn-iptables
+}
+trap iptablescleanup EXIT
+
+echo -e "\nTest $0 \n"
+set -e
+set -x
+
+launch_iptables
+
+ping $ip -W 1 -c 2 -W 2
+
+pcn-iptables -P INPUT ACCEPT
+pcn-iptables -P OUTPUT ACCEPT
+
+ping $ip -W 1 -c 2 -W 2
+
+pcn-iptables -P INPUT DROP
+
+test_fail ping $ip -W 1 -c 2 -W 2
+
+pcn-iptables -P INPUT ACCEPT
+
+polycubectl pcn-iptables set horus=ON
+
+ping $ip -W 1 -c 2 -W 2
+
+pcn-iptables -A INPUT -s $ip -j DROP
+
+test_fail ping $ip -W 1 -c 2 -W 2
+
+pcn-iptables -D INPUT -s $ip -j DROP
+pcn-iptables -S INPUT
+pcn-iptables -L INPUT
+
+ping $ip -W 1 -c 2 -W 2
